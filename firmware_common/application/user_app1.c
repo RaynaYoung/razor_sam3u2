36d6a0816a7dc9b248277ffa0160bc2871b9fa55
RaynaYoung <rayna.young1@ucalgary.ca>
2023-11-09 20:18:26 -0700

    Update user_app1.c

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -92,6 +92,21 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
+  
+  LedOff(PURPLE);
+  LedOff(WHITE);
+  LedOff(YELLOW);
+  LedOff(ORANGE);
+  LedOff(CYAN);
+  LedOff(BLUE);
+  LedOff(RED);
+  LedOff(GREEN);
+  
+  LedOn(LCD_RED);
+  LedOn(LCD_GREEN);
+  LedOn(LCD_BLUE);
+  
+  
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -140,7 +155,49 @@ State Machine Function Definitions
 /* What does this state do? */
 static void UserApp1SM_Idle(void)
 {
+    static u16 u16BlinkCount = 0;
+    static u8 u8Counter = 0;
     
+    u16BlinkCount++;
+    if(u16BlinkCount == 250){
+      u16BlinkCount = 0;
+      u8Counter++;
+      if(u8Counter == 16){
+        u8Counter = 0;
+      }
+      
+      if(u8Counter & 0x01){
+        LedOn(RED);
+      }
+      else{
+        LedOff(RED);
+      }
+      
+      
+         if(u8Counter & 0x02){
+        LedOn(ORANGE);
+      }
+      else{
+        LedOff(ORANGE);
+      }
+      
+      
+         if(u8Counter & 0x04){
+        LedOn(YELLOW);
+      }
+      else{
+        LedOff(YELLOW);
+      }
+      
+      
+         if(u8Counter & 0x08){
+        LedOn(GREEN);
+      }
+      else{
+        LedOff(GREEN);
+      }
+      
+    }
 } /* end UserApp1SM_Idle() */
      
 
